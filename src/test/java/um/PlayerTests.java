e80c47317f147d7ee392311c816955da4b6b0c2a
Jack Piscopo <jackpiscopo1999@gmail.com>
2020-05-02 17:13:05 +0200

    Added test for setposition when true

--- src/test/java/um/PlayerTests.java
+++ src/test/java/um/PlayerTests.java
@@ -9,6 +9,7 @@ public class PlayerTests {
     @Test
     public void setPositionReturnsTrue() {
         Player player = new Player();
-
+        Position position = new Position(2, 3);
+        player.setPosition(position);
     }
 }
